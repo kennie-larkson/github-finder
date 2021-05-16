afd269e31e5e4b9607d574a3f868387fa30dd534
kennie-larkson <materialkenny@gmail.com>
2021-05-16 09:43:45 +0100

    used prettier formatter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar"
-
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
-   
- render() {
-   
-  return (
-   
-    <div className="App" >
-      <Navbar />
-    </div>
-   
-  );
- }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
